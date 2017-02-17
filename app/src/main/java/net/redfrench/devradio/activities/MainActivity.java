b9b9cb1c1a79cdbf168d5cfd950ec2fafb6523d5
Red-French <red@redfrench.net>
2017-02-16 23:58:13 -0600

    add recyclerview and cardview dependencies

---
+++ app/src/main/java/net/redfrench/devradio/activities/MainActivity.java
@@ -0,0 +1,17 @@
+package net.redfrench.devradio.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import net.redfrench.devradio.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        
+    }
+}
