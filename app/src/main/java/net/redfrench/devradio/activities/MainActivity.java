4a20d61a1fbc1f573dafb8a7c28c198cca5d35fd
Red-French <red@redfrench.net>
2017-02-17 00:02:14 -0600

    initial commit

--- app/src/main/java/net/redfrench/devradio/activities/MainActivity.java
+++ app/src/main/java/net/redfrench/devradio/activities/MainActivity.java
@@ -1,9 +1,11 @@
 package net.redfrench.devradio.activities;
 
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import net.redfrench.devradio.R;
+import net.redfrench.devradio.fragments.DaddyFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +14,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        
+        FragmentManager fragMgr = getSupportFragmentManager();
+        DaddyFragment daddyFragment = (DaddyFragment)fragMgr.findFragmentById(R.id.container_main);  // cast the returned fragment into a DaddyFragment
+                                                                                                     // because a regular fragment will be returned
+
+        if (daddyFragment == null) {  // if the fragment has previously loaded, it will be in memory. Otherwise, fragment will be null.
+            daddyFragment = DaddyFragment.newInstance("blah", "kah"); // new DaddyFragment will be created in newInstance method
+            fragMgr.beginTransaction()  // anytime you work with the FragmentManager, you begin a transaction
+                    .add(R.id.container_main, daddyFragment)  // put daddyFragment in the 'container_main' fragment
+                    .commit();  // always end by committing
+        }
+
     }
 }
--- app/src/main/java/net/redfrench/devradio/fragments/DaddyFragment.java
+++ app/src/main/java/net/redfrench/devradio/fragments/DaddyFragment.java
@@ -60,7 +60,7 @@ public class DaddyFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        return inflater.inflate(R.layout.fragment_daddy, container, false);  // inflate 'fragment_daddy.xml'
     }
 
 }
