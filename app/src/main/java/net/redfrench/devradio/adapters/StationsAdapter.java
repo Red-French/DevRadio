8f57d092bafbb38114cec6bfa84ee83e8a6f7d80
Red-French <red@redfrench.net>
2017-02-19 20:21:09 -0600

    add 'debuggable true'

---
+++ app/src/main/java/net/redfrench/devradio/adapters/StationsAdapter.java
@@ -0,0 +1,8 @@
+package net.redfrench.devradio.adapters;
+
+/**
+ * Created by user on 2/19/17.
+ */
+
+public class StationsAdapter {
+}
---
+++ app/src/main/java/net/redfrench/devradio/holders/StationViewHolder.java
@@ -0,0 +1,8 @@
+package net.redfrench.devradio.holders;
+
+/**
+ * Created by user on 2/19/17.
+ */
+
+public class StationViewHolder {
+}
