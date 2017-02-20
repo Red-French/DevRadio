36f23d50469dfd2822ac8da96e4ad249d348d19d
Red-French <red@redfrench.net>
2017-02-19 20:25:38 -0600

    initial commit

--- app/src/main/java/net/redfrench/devradio/adapters/StationsAdapter.java
+++ app/src/main/java/net/redfrench/devradio/adapters/StationsAdapter.java
@@ -1,8 +1,31 @@
 package net.redfrench.devradio.adapters;
 
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+import net.redfrench.devradio.holders.StationViewHolder;
+
+import java.util.List;
+
 /**
  * Created by user on 2/19/17.
  */
 
-public class StationsAdapter {
+public class StationsAdapter extends RecyclerView.Adapter<StationViewHolder>{
+
+    @Override
+    public void onBindViewHolder(StationViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    @Override
+    public StationViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
 }
--- app/src/main/java/net/redfrench/devradio/holders/StationViewHolder.java
+++ app/src/main/java/net/redfrench/devradio/holders/StationViewHolder.java
@@ -1,8 +1,15 @@
 package net.redfrench.devradio.holders;
 
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
 /**
  * Created by user on 2/19/17.
  */
 
-public class StationViewHolder {
+public class StationViewHolder extends RecyclerView.ViewHolder {
+
+    public StationViewHolder(View itemView) {
+        super(itemView);
+    }
 }
