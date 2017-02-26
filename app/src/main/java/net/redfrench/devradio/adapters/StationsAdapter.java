14895ea2e61283c2f294d97c2b9229b8499a78ab
Red-French <red@redfrench.net>
2017-02-26 17:42:27 -0600

    write onBindViewHolder(), add stations.size() to getItemCount(), and inflate card_station view in onCreateViewHolder() and pass it to StationViewHolder()

--- app/src/main/java/net/redfrench/devradio/adapters/StationsAdapter.java
+++ app/src/main/java/net/redfrench/devradio/adapters/StationsAdapter.java
@@ -1,13 +1,15 @@
 package net.redfrench.devradio.adapters;
 
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 
+import net.redfrench.devradio.R;
 import net.redfrench.devradio.holders.StationViewHolder;
 import net.redfrench.devradio.model.Station;
 
 import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Created by user on 2/19/17.
@@ -19,22 +21,29 @@ public class StationsAdapter extends RecyclerView.Adapter<StationViewHolder>{  /
     private ArrayList<Station> stations;  // <Station> is the model
 
     public StationsAdapter(ArrayList<Station> stations) {  // constructor
-        this.stations = stations;
+        this.stations = stations;  // saves data in the ArrayList, created above
     }
 
+    // pass data into the view holder
     @Override
-    public void onBindViewHolder(StationViewHolder holder, int position) {
-
+    public void onBindViewHolder(StationViewHolder holder, int position) {  // 'holder' is passing in the card, 'position' is the index
+        Station station = stations.get(position);  // 'stations' is the ArrayList
+        holder.updateUI(station); // updateUI() is in StationViewHolder.java
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return stations.size();  // for each station, onCreateViewHolder() will be called and will create a card/view for it
     }
 
+    // every time a view is needed, this will be called, and a view will be created
     @Override
     public StationViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+
+        // inflate the card_station view (parent is the context, which is the recyclerView, which is content_stations_recycler.xml)
+        View stationCard = LayoutInflater.from(parent.getContext()).inflate(R.layout.card_station, parent, false);  // inflate the view from the parent context
+
+        return new StationViewHolder(stationCard);
     }
 
 }
