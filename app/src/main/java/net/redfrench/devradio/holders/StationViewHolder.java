1a91f25f0b6e6d2dab999b2b2b19e1f2d5f43a82
Red-French <red@redfrench.net>
2017-02-26 17:47:36 -0600

    add reference to views in StationViewHolder() constructor and write updateUI()

--- app/src/main/java/net/redfrench/devradio/holders/StationViewHolder.java
+++ app/src/main/java/net/redfrench/devradio/holders/StationViewHolder.java
@@ -1,7 +1,13 @@
 package net.redfrench.devradio.holders;
 
+import android.media.Image;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import net.redfrench.devradio.R;
+import net.redfrench.devradio.model.Station;
 
 /**
  * Created by user on 2/19/17.
@@ -9,7 +15,27 @@ import android.view.View;
 
 public class StationViewHolder extends RecyclerView.ViewHolder {
 
+    private ImageView mainImage;
+    private TextView titleTextView;
+
+
     public StationViewHolder(View itemView) {  // constructor (a java entry point that's called when you create a new instance of this class)
+
         super(itemView);
+
+        this.mainImage = (ImageView)itemView.findViewById(R.id.main_image);
+        this.titleTextView = (TextView)itemView.findViewById(R.id.main_text);
+    }
+
+    // called when the UI needs to be updated
+    public void updateUI(Station station) {  // every time a new view (card_station.xml) hits the screen, pass the station into it
+        // change image
+        String uri = station.getImgUri();  // getImgUri() is in the model (Station.java)
+        int resource = mainImage.getResources().getIdentifier(uri, null, mainImage.getContext().getPackageName());  // can only get the package
+                                                                                                            // from the mainActivity context
+                                                                                                            // which is why context is gotten
+                                                                                                            // from mainImage
+        mainImage.setImageResource(resource);  // setImageResource() takes an ID
+        titleTextView.setText(station.getStationTitle());
     }
 }
