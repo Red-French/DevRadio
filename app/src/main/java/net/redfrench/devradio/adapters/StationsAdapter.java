3963e3e0f077879081d0da5f023832f0e6331190
Red-French <red@redfrench.net>
2017-02-25 23:28:11 -0600

    add StationsAdapter() constructor

--- app/src/main/java/net/redfrench/devradio/adapters/StationsAdapter.java
+++ app/src/main/java/net/redfrench/devradio/adapters/StationsAdapter.java
@@ -4,14 +4,23 @@ import android.support.v7.widget.RecyclerView;
 import android.view.ViewGroup;
 
 import net.redfrench.devradio.holders.StationViewHolder;
+import net.redfrench.devradio.model.Station;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by user on 2/19/17.
  */
 
-public class StationsAdapter extends RecyclerView.Adapter<StationViewHolder>{
+//  The adapter will grab all the data from your data source, shove it in the viewHolder, and the viewHolder will go inside the recyclerView
+public class StationsAdapter extends RecyclerView.Adapter<StationViewHolder>{  // RecyclerView needs to know the viewHolder you'll use
+
+    private ArrayList<Station> stations;  // <Station> is the model
+
+    public StationsAdapter(ArrayList<Station> stations) {  // constructor
+        this.stations = stations;
+    }
 
     @Override
     public void onBindViewHolder(StationViewHolder holder, int position) {
