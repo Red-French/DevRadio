6993d21816447297ed425abcf044c977cdbc7ffa
Red-French <red@redfrench.net>
2017-02-26 19:22:04 -0600

    write loadDetailsScreen()

--- app/src/main/java/net/redfrench/devradio/activities/MainActivity.java
+++ app/src/main/java/net/redfrench/devradio/activities/MainActivity.java
@@ -6,13 +6,28 @@ import android.os.Bundle;
 
 import net.redfrench.devradio.R;
 import net.redfrench.devradio.fragments.DaddyFragment;
+import net.redfrench.devradio.fragments.DetailsFragment;
+import net.redfrench.devradio.model.Station;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static MainActivity getMainActivity() {
+        return mainActivity;
+    }
+
+    private static void setMainActivity(MainActivity mainActivity) {
+        MainActivity.mainActivity = mainActivity;  // to make mainActivity acessible from anywhere
+    }
+
+    private static MainActivity mainActivity;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MainActivity.setMainActivity(this);  // now have access to mainActivity from anywhere
 
         FragmentManager fragMgr = getSupportFragmentManager();
         DaddyFragment daddyFragment = (DaddyFragment)fragMgr.findFragmentById(R.id.container_main);  // cast the returned fragment into a DaddyFragment
@@ -24,6 +39,13 @@ public class MainActivity extends AppCompatActivity {
                     .add(R.id.container_main, daddyFragment)  // put daddyFragment in the 'container_main' fragment
                     .commit();  // always end by committing
         }
+    }
 
+    public void loadDetailsScreen(Station selectedStation) {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.container_main, new DetailsFragment())
+                .addToBackStack(null)
+                .commit();
     }
 }
